e4352fac5337a8299b1ec21eefb014f55be20cc4
Andrew <yham023@aucklanduni.ac.nz>
2022-09-01 13:06:47 +1200

    Added ServiceWorker (PWA)

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+  );
+
+  workbox.routing.registerRoute(                    //access to workbox
+    ({request}) => request.destination === 'image', //use routing module to register route that matches all the image files in our project
+    new workbox.strategies.CacheFirst()             // caching strategy after image files match. Cachefirst attempts to read from cache instead of going over network first.
+  );    
+
+  const cacheName = 'crypto-v1';
+  const staticAssets = [
+    './',
+    './src/App.cssasds'
+  ]
